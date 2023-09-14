034a256c752f0f2513294c06fbb0181054015bbf
Sang Phạm <youlovelong123@gmail.com>
2023-09-14 09:37:17 +0700

    Json Test
    --Update Flutter

--- test/src/authentication/data/models/user_model_test.dart
+++ test/src/authentication/data/models/user_model_test.dart
@@ -1,13 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter_bloc_clean_arh_project/src/authentication/data/models/user_model.dart';
 import 'package:flutter_bloc_clean_arh_project/src/authentication/domain/entities/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('should be subclass of [User] entity', () {
-    // Arrange
     const tModel = UserModel.empty();
-
-    // Act
 
     // Assert
     expect(tModel, isA<User>());
@@ -16,6 +15,10 @@ void main() {
   group('fromMap', () {
     test('should return a [UserModel] with the right data', () {
       // Arange
+      final tJson = File('test/fixtures/user.json').readAsStringSync();
+      print(tJson);
+      // Act
+      // final result = UserModel.fromMap(map);
     });
   });
 }
