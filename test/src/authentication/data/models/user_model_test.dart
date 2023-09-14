c7d96e09af8229ee218b7567c3be91900480dcaf
Sang Phạm <youlovelong123@gmail.com>
2023-09-14 10:04:20 +0700

    Unit test & json
    --Update Flutter

--- test/src/authentication/data/models/user_model_test.dart
+++ test/src/authentication/data/models/user_model_test.dart
@@ -1,10 +1,14 @@
-import 'dart:io';
-
+import 'dart:convert';
+import 'package:flutter_bloc_clean_arh_project/core/utils/typedef.dart';
 import 'package:flutter_bloc_clean_arh_project/src/authentication/data/models/user_model.dart';
 import 'package:flutter_bloc_clean_arh_project/src/authentication/domain/entities/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../../../../fixtures/fixture_reader.dart';
+
 void main() {
+  const tModel = UserModel.empty();
+
   test('should be subclass of [User] entity', () {
     const tModel = UserModel.empty();
 
@@ -12,13 +16,15 @@ void main() {
     expect(tModel, isA<User>());
   });
 
+  final tJson = fixture('user.json');
+  final tMap = jsonDecode(tJson) as DataMap;
+
   group('fromMap', () {
     test('should return a [UserModel] with the right data', () {
       // Arange
-      final tJson = File('test/fixtures/user.json').readAsStringSync();
-      print(tJson);
       // Act
-      // final result = UserModel.fromMap(map);
+      final result = UserModel.fromMap(tMap);
+      expect(result, equals(tModel));
     });
   });
 }
